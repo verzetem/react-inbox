eaf321e7c39fb70b0fdfddda13b575ac86c1de05
chris <verzetem@gmail.com>
2018-10-21 21:21:13 -0600

    this version works

--- src/components/Message.js
+++ src/components/Message.js
@@ -2,9 +2,13 @@ import React, { Component } from 'react';
 
 class Message extends Component {
 
-	handleClick(e) {
+	msgReadToggle(e) {
 		console.log("message clicked", e.persist(), e)
 		// (e.target.attributes.isRead.value !== "true") ? e.target.classList. : console.log("true, do nothing")
+		// (message.read !== true) ? e.target.classList
+ }
+ startToggle(e) {
+ 	console.log("star toggle", e)
  }
 
 render() {
@@ -12,46 +16,47 @@ render() {
 
     <div>
 
-			{console.log("messageData", this.props.messageData)}
+			{ console.log("messageData", this.props.messageData) }
 			{ this.props.messageData.map((message) => 
 				(message.read !== true) ? 
-					<div className="row message unread">
-			  <div className="col-xs-1">
-			    <div className="row">
-			      <div className="col-xs-2">
-			        <input type="checkbox" />
-			      </div>
-			      <div className="col-xs-2">
-			        <i className="star fa fa-star-o"></i>
-			      </div>
-			    </div>
-			  </div>
-			  <div className="col-xs-10">
-			    <a href="#">
-			      <p onClick={this.handleClick} id={message.id} labels={message.labels} starred={message.starred.toString()} isRead={message.read.toString()}>{message.subject}</p>
-			    </a>
-			  </div>
-			</div>
-			:
+					<div onClick={this.msgReadToggle} isread={message.read.toString()} className="row message unread">
+					  <div className="col-xs-1">
+					    <div className="row">
+					      <div className="col-xs-2">
+					        <input type="checkbox" />
+					      </div>
+					      <div className="col-xs-2">
+					        <i onClick={this.startToggle} className="star fa fa-star-o"></i>
+					      </div>
+					    </div>
+					  </div>
+					  <div className="col-xs-10">
+					    <a href="#">
+					      <p id={message.id} labels={message.labels} starred={message.starred.toString()}>{message.subject}</p>
+					    </a>
+					  </div>
+					</div>
+				:
 					<div className="row message read">
-			  <div className="col-xs-1">
-			    <div className="row">
-			      <div className="col-xs-2">
-			        <input type="checkbox" />
-			      </div>
-			      <div className="col-xs-2">
-			        <i className="star fa fa-star-o"></i>
-			      </div>
-			    </div>
-			  </div>
-			  <div className="col-xs-10">
-			    <a href="#">
-			      <p onClick={this.handleClick} id={message.id} labels={message.labels} starred={message.starred.toString()} isRead={message.read.toString()}>{message.subject}</p>
-			    </a>
-			  </div>
-			</div>
+					  <div className="col-xs-1">
+					    <div className="row">
+					      <div className="col-xs-2">
+					        <input type="checkbox" />
+					      </div>
+					      <div className="col-xs-2">
+					        <i className="star fa fa-star-o"></i>
+					      </div>
+					    </div>
+					  </div>
+					  <div className="col-xs-10">
+					    <a href="#">
+					      <p onClick={this.handleClick} id={message.id} labels={message.labels} starred={message.starred.toString()} isread={message.read.toString()}>{message.subject}</p>
+					    </a>
+					  </div>
+					</div>,
+					
 	      )}
-
+				
     </div>
 
   );
